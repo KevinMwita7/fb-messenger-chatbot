073f6b71b9e29a2943f48ac3c0df1fadb65107b5
KevinMwita7 <33288033+KevinMwita7@users.noreply.github.com>
2020-03-11 23:32:35 +0300

    Update send-messages.js

--- utils/send-messages.js
+++ utils/send-messages.js
@@ -4,11 +4,13 @@ const senderActions = require("../services/sender-actions");
 module.exports = function sendMessages(sender_psid, responses) {
     let delay = 0;
     if(Array.isArray(responses)) {
-        responses.forEach((response, index) => {
-            FacebookApi.callSendAPI(sender_psid, response);
-            // hide the typing indicator
-            if(index === responses.length - 1) senderActions(sender_psid, "typing_off");
-            delay += 1;
-        }, delay * 2000 );
+        setTimeout(() => {
+            responses.forEach((response, index) => {
+                FacebookApi.callSendAPI(sender_psid, response);
+                // hide the typing indicator
+                if(index === responses.length - 1) senderActions(sender_psid, "typing_off");
+                delay += 1;
+            });
+        }, delay * 2000)
     }
 };
